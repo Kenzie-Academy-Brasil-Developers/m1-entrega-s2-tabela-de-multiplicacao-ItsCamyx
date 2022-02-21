57e6e041125aaf112b0450d5e95be76e4d4ad754
ItsCamyx <96155623+ItsCamyx@users.noreply.github.com>
2022-02-21 17:10:55 -0300

    adcMult

---
+++ script.js
@@ -0,0 +1,16 @@
+function multiplicationTable(tamanho) {
+  const arr = [];
+  for (let i = 0; i <= tamanho; i++) {
+    const linha = [];
+    for (let j = 0; j <= tamanho; j++) {
+      linha.push(i * j);
+    }
+
+    arr[i] = linha;
+  }
+  console.table(arr);
+  for (let i = 0; i <= tamanho; i++) {
+    console.log(arr[i][4]);
+  }
+  return arr;
+}
