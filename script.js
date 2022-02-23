6aefd74d94a88e8112f9adfa23acebb8c777e3b5
ItsCamyx <96155623+ItsCamyx@users.noreply.github.com>
2022-02-22 22:49:55 -0300

    AtualFim

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
+//propriedade
 function multiplicationTable(tamanho) {
   const arr = [];
+  // para i = 0 até i maior igual tamanho acrescentar +1
   for (let i = 0; i <= tamanho; i++) {
     const linha = [];
+    // para j = 0 até j maior igual tamanho, acrescentar +1
     for (let j = 0; j <= tamanho; j++) {
+      //Assim que termina o j o i acrescenta mais um e multiplica por outro numero do j
       linha.push(i * j);
     }
-
+    //Valor de array tem i que recebe o valor de linha
     arr[i] = linha;
   }
   console.table(arr);
